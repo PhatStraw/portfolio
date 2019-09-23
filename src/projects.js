7926d80effd1f8ee1b5903dff2d5ee17dec749ac
Kevin Sims <kevindsims1@gmail.com>
2019-09-23 18:26:06 -0500

    updated html

--- src/projects.js
+++ src/projects.js
@@ -16,66 +16,90 @@ class Projects extends Component {
 
             <div className="projects">
                 <div className="item">
-                <a href="#">
+                <a href="https://donor-management-fe.netlify.com/signup"
+                    target="_blank"
+                    rel="noopener noreferer">
                     <img src="src/img/project1.jpg"/>
                 </a>
 
-                <a className="btn-light" href="#">
+                <a className="btn-light" href="https://donor-management-fe.netlify.com/signup"  target="_blank"
+                    rel="noopener noreferer">
                     <i className="fas fa-eye"> </i>Project
                 </a>
-                <a className="btn-dark" href="#">
+                <a className="btn-dark" href="https://github.com/kevinsims1/donormanagement-fe"  target="_blank"
+                    rel="noopener noreferer">
                     <i className="fab fa-github"></i>Github
                 </a>
                 </div>          
            
                 <div className="item">
-                <a>
+                <a  target="_blank"
+                    rel="noopener noreferer"
+                    href="">
                 <img src="src/img/project2.jpg"/>
                 </a>
 
-                <a className="btn-light">
+                <a className="btn-light"  target="_blank"
+                    rel="noopener noreferer">
                     <i className="fas fa-eye"> </i>Project
                 </a>
-                <a className="btn-dark">
+                <a className="btn-dark"  target="_blank"
+                    rel="noopener noreferer">
                     <i className="fab fa-github"></i>Github
                 </a>
                 </div>                
             
                 <div className="item">
-                <a>
+                <a  target="_blank"
+                    rel="noopener noreferer"> 
                      <img src="src/img/project3.jpg"/>
                 </a>
 
-                <a className="btn-light">
+                <a className="btn-light"
+                     target="_blank"
+                     rel="noopener noreferer">
                     <i className="fas fa-eye"> </i>Project
                 </a>
-                <a className="btn-dark">
+                <a className="btn-dark"
+                     target="_blank"
+                     rel="noopener noreferer">
                     <i className="fab fa-github"></i>Github
                 </a>
                 </div>             
             
                 <div className="item">
-                <a>
+                <a
+                 target="_blank"
+                 rel="noopener noreferer">
                      <img src="src/img/project4.jpg"/>
                 </a>
 
-                <a className="btn-light">
+                <a className="btn-light"
+                     target="_blank"
+                     rel="noopener noreferer">
                     <i className="fas fa-eye"> </i>Project
                 </a>
-                <a className="btn-dark">
+                <a className="btn-dark"
+                     target="_blank"
+                     rel="noopener noreferer">
                     <i className="fab fa-github"></i>Github
                 </a>
                 </div>     
             
                 <div className="item">
-                   <a>
+                   <a target="_blank"
+                    rel="noopener noreferer"> 
                      <img src="src/img/project5.jpg"/>
                     </a>
 
-                    <a className="btn-light">
+                    <a className="btn-light"
+                         target="_blank"
+                         rel="noopener noreferer">
                         <i className="fas fa-eye"> </i>Project
                     </a>
-                    <a className="btn-dark">
+                    <a className="btn-dark"
+                         target="_blank"
+                         rel="noopener noreferer">
                         <i className="fab fa-github"></i>Github
                     </a> 
                 </div>        
