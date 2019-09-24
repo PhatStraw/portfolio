b786d481826424fd86c13228e0f08b7cd77a8ce2
Kevin Sims <kevindsims1@gmail.com>
2019-09-24 10:11:39 -0500

    updated projects page

--- src/projects.js
+++ src/projects.js
@@ -15,80 +15,80 @@ class Projects extends Component {
             </h2>
 
             <div className="projects">
-                <div className="item">
-                <a href="https://donor-management-fe.netlify.com/signup"
-                    target="_blank"
-                    rel="noopener noreferer">
-                </a>
-
-                <a className="btn-light" href="https://donor-management-fe.netlify.com/signup"  target="_blank"
-                    rel="noopener noreferer">
-                    <i className="fas fa-eye"> </i>Donor Management
-                </a>
-                <a className="btn-dark" href="https://github.com/kevinsims1/donormanagement-fe"  target="_blank"
-                    rel="noopener noreferer">
-                    <i className="fab fa-github"></i>Github
-                </a>
-                </div>          
-           
-                <div className="item">
-                <a  target="_blank"
-                    rel="noopener noreferer"
-                    href="https://github.com/kevinsims1/project-board">
-                </a>
-
-                <a className="btn-light"  target="_blank"
-                    rel="noopener noreferer"
-                    href="https://github.com/kevinsims1/project-board">
-                    <i className="fas fa-eye"> </i>Project Board
-                </a>
-                <a className="btn-dark"  target="_blank"
-                    rel="noopener noreferer"
-                    href="https://github.com/kevinsims1/project-board">
-                    <i className="fab fa-github"></i>Github
-                </a>
-                </div>                
-            
-                <div className="item">
-                <a  target="_blank"
-                    rel="noopener noreferer"
-                    href="https://github.com/kevinsims1/OpenSource-marketplace-BE"> 
-                        </a>
-
-                <a className="btn-light"
-                     target="_blank"
-                     rel="noopener noreferer"
-                     href="https://github.com/kevinsims1/OpenSource-marketplace-BE">
-                    <i className="fas fa-eye"> </i>Open Source Marketplace Api
-                </a>
-                <a className="btn-dark"
-                     target="_blank"
-                     rel="noopener noreferer"
-                     href="https://github.com/kevinsims1/OpenSource-marketplace-BE">
-                    <i className="fab fa-github"></i>Github
-                </a>
-                </div>             
-            
-                <div className="item">
+            <div className="item">
                 <a
                  target="_blank"
-                 rel="noopener noreferer"
+                 rel="noopener noreferrer"
                  href="https://github.com/labs15-rv-life">
                         </a>
 
                 <a className="btn-light"
                      target="_blank"
-                     rel="noopener noreferer"
-                     href="https://github.com/labs15-rv-life">
+                     rel="noopener noreferrer"
+                     href="https://www.rvnav.com/">
                     <i className="fas fa-eye"> </i>RV Navigator
                 </a>
                 <a className="btn-dark"
                      target="_blank"
-                     rel="noopener noreferer"
+                     rel="noopener noreferrer"
                      href="https://github.com/labs15-rv-life">
                     <i className="fab fa-github"></i>Github
                 </a>
-                </div>     
+                </div>           
+           
+                <div className="item">
+                <a  target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://github.com/kevinsims1/project-board">
+                </a>
+
+                <a className="btn-light"  target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://github.com/kevinsims1/project-board">
+                    <i className="fas fa-eye"> </i>Project Board
+                </a>
+                <a className="btn-dark"  target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://github.com/kevinsims1/project-board">
+                    <i className="fab fa-github"></i>Github
+                </a>
+                </div> 
+
+                <div className="item">
+                <a href="https://donor-management-fe.netlify.com/signup"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                </a>
+
+                <a className="btn-light" href="https://donor-management-fe.netlify.com/signup"  target="_blank"
+                    rel="noopener noreferrer">
+                    <i className="fas fa-eye"> </i>Donor Management
+                </a>
+                <a className="btn-dark" href="https://github.com/kevinsims1/donormanagement-fe"  target="_blank"
+                    rel="noopener noreferrer">
+                    <i className="fab fa-github"></i>Github
+                </a>
+                </div>                   
+            
+                <div className="item">
+                <a  target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://github.com/kevinsims1/OpenSource-marketplace-BE"> 
+                        </a>
+
+                <a className="btn-light"
+                     target="_blank"
+                     rel="noopener noreferrer"
+                     href="https://github.com/kevinsims1/OpenSource-marketplace-BE">
+                    <i className="fas fa-eye"> </i>Open Source Marketplace Api
+                </a>
+                <a className="btn-dark"
+                     target="_blank"
+                     rel="noopener noreferrer"
+                     href="https://github.com/kevinsims1/OpenSource-marketplace-BE">
+                    <i className="fab fa-github"></i>Github
+                </a>
+                </div>             
             </div>
             </main>
             <footer id="main-footer">
