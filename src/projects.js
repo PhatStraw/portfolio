7a3d0f8a85129ed1594ea367e13e80a18a63bdfc
kevinsims1 <kevinsims1@outlook.com>
2020-01-04 09:36:00 -0600

    updated projects and theme

--- src/projects.js
+++ src/projects.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
-
+import Rvnav from './img/rvnavimg.png';
+import Weatherapp from './img/weatherapp.png';
+import Youtube from './img/youtube.jpg';
+import DonorTransparency from './img/DonorT.png';
+import SautiAM from './img/SautiAM.jpg';
+import Portfolio from './img/Portfolio.jpg';
 class Projects extends Component {
     render(){
         return(
@@ -15,8 +20,8 @@ class Projects extends Component {
 
             <div className="projects">
             <div className="item">
-                <a href="https://github.com/labs15-rv-life">
-                 <img src="#" alt=''/>
+                <a href="https://rvnav.com">
+                 <img src={Rvnav} alt=''/>
                 </a>
 
                 <a className="btn-light"
@@ -28,7 +33,7 @@ class Projects extends Component {
                 <a className="btn-dark"
                      target="_blank"
                      rel="noopener noreferrer"
-                     href="https://github.com/labs15-rv-life">
+                     href="https://github.com/Lambda-School-Labs/rv-nav-fe">
                     <i className="fab fa-github"></i>Github
                 </a>
                 </div>           
@@ -36,18 +41,18 @@ class Projects extends Component {
                 <div className="item">
                 <a  target="_blank"
                     rel="noopener noreferrer"
-                    href="https://github.com/kevinsims1/project-board">
-                    <img src="#" alt=''/>
+                    href="https://kevinsims1.github.io/u-tube/">
+                    <img src={Youtube} alt=''/>
                 </a>
 
                 <a className="btn-light"  target="_blank"
                     rel="noopener noreferrer"
-                    href="https://github.com/kevinsims1/project-board">
-                    <i className="fas fa-eye"> </i>Project Board
+                    href="https://kevinsims1.github.io/u-tube/">
+                    <i className="fas fa-eye"> </i>Youtube Clone
                 </a>
                 <a className="btn-dark"  target="_blank"
                     rel="noopener noreferrer"
-                    href="https://github.com/kevinsims1/project-board">
+                    href="https://github.com/kevinsims1/u-tube">
                     <i className="fab fa-github"></i>Github
                 </a>
                 </div> 
@@ -56,7 +61,7 @@ class Projects extends Component {
                 <a href="https://donor-management-fe.netlify.com/signup"
                     target="_blank"
                     rel="noopener noreferrer">
-                    <img src="#" alt=''/>
+                    <img src={DonorTransparency} alt=''/>
                 </a>
 
                 <a className="btn-light" href="https://donor-management-fe.netlify.com/signup"  target="_blank"
@@ -72,14 +77,14 @@ class Projects extends Component {
                 <div className="item">
                 <a  target="_blank"
                     rel="noopener noreferrer"
-                    href="https://github.com/kevinsims1/OpenSource-marketplace-BE"> 
-                    <img src="../img/project1.jpg" alt=''/>
+                    href="https://price-api.sautiafrica.org"> 
+                    <img src={SautiAM} alt=''/>
                         </a>
 
                 <a className="btn-light"
                      target="_blank"
                      rel="noopener noreferrer"
-                     href="https://github.com/kevinsims1/OpenSource-marketplace-BE">
+                     href="https://price-api.sautiafrica.org">
                     <i className="fas fa-eye"> </i>Open Source Marketplace Api
                 </a>
                 <a className="btn-dark"
@@ -88,7 +93,49 @@ class Projects extends Component {
                      href="https://github.com/kevinsims1/OpenSource-marketplace-BE">
                     <i className="fab fa-github"></i>Github
                 </a>
-                </div>             
+                </div>   
+                
+                <div className="item">
+                <a  target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://kevinsims1.github.io/weather-app/"> 
+                    <img src={Weatherapp} alt=''/>
+                        </a>
+
+                <a className="btn-light"
+                     target="_blank"
+                     rel="noopener noreferrer"
+                     href="https://kevinsims1.github.io/weather-app/">
+                    <i className="fas fa-eye"> </i>Weather App
+                </a>
+                <a className="btn-dark"
+                     target="_blank"
+                     rel="noopener noreferrer"
+                     href="https://github.com/kevinsims1/weather-app">
+                    <i className="fab fa-github"></i>Github
+                </a>
+                </div>   
+
+                <div className="item">
+                <a  target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://kevinsims.dev"> 
+                    <img src={Portfolio} alt=''/>
+                        </a>
+
+                <a className="btn-light"
+                     target="_blank"
+                     rel="noopener noreferrer"
+                     href="https://kevinsims.dev">
+                    <i className="fas fa-eye"> </i>Portfolio Project
+                </a>
+                <a className="btn-dark"
+                     target="_blank"
+                     rel="noopener noreferrer"
+                     href="https://github.com/kevinsims1/portfolio">
+                    <i className="fab fa-github"></i>Github
+                </a>
+                </div>          
             </div>
             </main>
             </>
